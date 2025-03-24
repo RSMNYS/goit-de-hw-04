1078c408744f4b3048953996fdc788bea16dfae5
Sergij Rylskyj <sergij.r@scopicsoftware.com>
2025-03-24 21:06:41 +0200

    initial commit

---
+++ main_3.py
@@ -0,0 +1,114 @@
+from pyspark.sql import SparkSession
+
+# Part 1
+# Створюємо сесію Spark
+# spark = SparkSession.builder \
+#     .master("local[*]") \
+#     .config("spark.sql.shuffle.partitions", "2") \
+#     .appName("MyGoitSparkSandbox") \
+#     .getOrCreate()
+#
+# # Завантажуємо датасет
+# nuek_df = spark.read \
+#     .option("header", "true") \
+#     .option("inferSchema", "true") \
+#     .csv('./nuek-vuh3.csv')
+#
+# nuek_repart = nuek_df.repartition(2)
+#
+# nuek_processed = nuek_repart \
+#     .where("final_priority < 3") \
+#     .select("unit_id", "final_priority") \
+#     .groupBy("unit_id") \
+#     .count()
+#
+# # Ось ТУТ додано рядок
+# nuek_processed = nuek_processed.where("count>2")
+#
+# nuek_processed.collect()
+#
+# input("Press Enter to continue...5")
+#
+# # Закриваємо сесію Spark
+# spark.stop()
+
+#Part 2
+
+# from pyspark.sql import SparkSession
+#
+# # Створюємо сесію Spark
+# spark = SparkSession.builder \
+#     .master("local[*]") \
+#     .config("spark.sql.shuffle.partitions", "2") \
+#     .appName("MyGoitSparkSandbox") \
+#     .getOrCreate()
+#
+# # Завантажуємо датасет
+# nuek_df = spark.read \
+#     .option("header", "true") \
+#     .option("inferSchema", "true") \
+#     .csv('./nuek-vuh3.csv')
+#
+# nuek_repart = nuek_df.repartition(2)
+#
+# nuek_processed = nuek_repart \
+#     .where("final_priority < 3") \
+#     .select("unit_id", "final_priority") \
+#     .groupBy("unit_id") \
+#     .count()
+#
+# # Проміжний action: collect
+# nuek_processed.collect()
+#
+# # Ось ТУТ додано рядок
+# nuek_processed = nuek_processed.where("count>2")
+#
+# nuek_processed.collect()
+#
+# input("Press Enter to continue...5")
+#
+# # Закриваємо сесію Spark
+# spark.stop()
+
+# Part 3
+
+from pyspark.sql import SparkSession
+
+# Створюємо сесію Spark
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .config("spark.sql.shuffle.partitions", "2") \
+    .appName("MyGoitSparkSandbox") \
+    .getOrCreate()
+
+# Завантажуємо датасет
+nuek_df = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv('./nuek-vuh3.csv')
+
+nuek_repart = nuek_df.repartition(2)
+
+nuek_processed_cached = nuek_repart \
+    .where("final_priority < 3") \
+    .select("unit_id", "final_priority") \
+    .groupBy("unit_id") \
+    .count() \
+    .cache()  # Додано функцію cache
+
+# Проміжний action: collect
+nuek_processed_cached.collect()
+
+# Ось ТУТ додано рядок
+nuek_processed = nuek_processed_cached.where("count>2")
+
+nuek_processed.collect()
+
+input("Press Enter to continue...5")
+
+# Звільняємо пям'ять від Dataframe
+nuek_processed_cached.unpersist()
+
+# Закриваємо сесію Spark
+spark.stop()
+
